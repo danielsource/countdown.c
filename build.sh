0a578f804825206ec76cbf99fc84e49285e48832
daniel.source <79308609+danielsource@users.noreply.github.com>
2024-07-01 18:38:47 -0300

    fix build

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ set -e
 
 sdl_headers_path=/usr/include/SDL2
 
-gcc -lSDL2 -std=c99 -Wall \
+gcc -std=c99 -Wall \
 	-DSDL_DISABLE_IMMINTRIN_H -I"$sdl_headers_path" \
-	-o countdown countdown.c
+	-o countdown countdown.c -lSDL2
